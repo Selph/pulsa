fabf1ed1615424caa7137594270629af6b348bca
Selph <72403805+Selph@users.noreply.github.com>
2022-08-29 22:05:18 +0000

    initial commit

---
+++ src/main/java/is/hi/hbv501g/h6/hugboverkefni/HugboVerkefniApplication.java
@@ -0,0 +1,13 @@
+package is.hi.hbv501g.h6.hugboverkefni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HugboVerkefniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HugboVerkefniApplication.class, args);
+    }
+
+}
---
+++ src/test/java/is/hi/hbv501g/h6/hugboverkefni/HugboVerkefniApplicationTests.java
@@ -0,0 +1,13 @@
+package is.hi.hbv501g.h6.hugboverkefni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HugboVerkefniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
