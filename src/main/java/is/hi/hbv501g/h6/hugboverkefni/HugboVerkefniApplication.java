bfc6be629cd0dd63145303f7c25da1ffddbe628e
Master Builder <gylfi@stebbi.is>
2022-09-15 21:21:48 +0000

    Aðeins að leika mér með Spring

--- src/main/java/is/hi/hbv501g/h6/hugboverkefni/HugboVerkefniApplication.java
+++ src/main/java/is/hi/hbv501g/h6/hugboverkefni/HugboVerkefniApplication.java
@@ -2,15 +2,22 @@ package is.hi.hbv501g.h6.hugboverkefni;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 // This is team member Abel
 // This is team member Gylfi
 
 @SpringBootApplication
+@RestController
 public class HugboVerkefniApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HugboVerkefniApplication.class, args);
     }
 
+    @GetMapping("")
+    public String start() {
+        return "Hello world";
+    }
 }
