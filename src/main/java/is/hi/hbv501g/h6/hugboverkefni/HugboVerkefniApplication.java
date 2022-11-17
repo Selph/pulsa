21e9a704e39af6a3e569dfeda11f96827236ec30
Selph <72403805+Selph@users.noreply.github.com>
2022-11-17 05:00:37 +0000

    war

--- src/main/java/is/hi/hbv501g/h6/hugboverkefni/HugboVerkefniApplication.java
+++ src/main/java/is/hi/hbv501g/h6/hugboverkefni/HugboVerkefniApplication.java
@@ -3,12 +3,13 @@ package is.hi.hbv501g.h6.hugboverkefni;
 import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class HugboVerkefniApplication {
+public class HugboVerkefniApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(HugboVerkefniApplication.class, args);
